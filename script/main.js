9ea8d2b5c5de04ffb24482b481b2ba3fb45575b9
Egnat <sergey_p_i@mail.ru>
2022-11-29 00:49:50 +0300

    initinal commit

---
+++ script/main.js
@@ -0,0 +1,29 @@
+/*const frontSideImg = document.querySelector('.new_certificate');
+frontSideImg.addEventListener('mousemove', function loop(event) {
+  event.stopPropagation();//чтоб событие не всплывало 
+  const frontSideImg = document.querySelector('.new_certificate img');
+    frontSideImg.style.cursor = "pointer";
+    //newText.innerHTML = "А эта тачка жене";
+    //newText.style.color = "#8be726";
+    //newText.style.left = "198px";
+  if (frontSideImg !== null) { // если элемент найден
+    frontSideImg.setAttribute('src', 'images/IMG_1.jpg');
+    return true;
+  } else { // иначе он не найден
+    return false;
+  }  
+});
+frontSideImg.addEventListener('mousemove', function loop(event) {
+  event.stopPropagation();//чтоб событие не всплывало 
+  const frontSideImg = document.querySelector('.new_certificate img');
+    frontSideImg.style.cursor = "pointer";
+    //newText.innerHTML = "А эта тачка жене";
+    //newText.style.color = "#8be726";
+    //newText.style.left = "198px";
+  if (frontSideImg !== null) { // если элемент найден
+    frontSideImg.setAttribute('src', 'images/IMG_2.jpg');
+    return true;
+  } else { // иначе он не найден
+    return false;
+  }  
+});*/
